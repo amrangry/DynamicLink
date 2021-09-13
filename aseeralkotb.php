b630426710047a6f13c12ff9d15698e501408751
Amr Elghadban <amr.elghadban@gmail.com>
2021-09-13 12:18:52 +0400

    Add files via upload
    sync with existing files

---
+++ aseeralkotb.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: AmrAngry
+ * Date: 02/09/21
+ * Time: 9:23 PM
+ * //https://www.w3docs.com/snippets/javascript/how-to-redirect-a-web-page-with-javascript.html
+ */
+$tagURL = 'bit.ly/3DGrDGi';
+header('Location: //' . $tagURL);
+
+exit();
