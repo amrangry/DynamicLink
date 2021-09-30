6e614c60e59cfbfe6edf8a91aecb0db58cc71a50
Amr Elghadban <amr.elghadban@gmail.com>
2021-09-30 12:48:02 +0400

    Update index.php
    Expo_2020_Dubai_Opening_Ceremony short link

--- index.php
+++ index.php
@@ -44,6 +44,8 @@ if (isset($url)) {
                         $tagURL = $default_adkatech_website;
                 }
             }
+        } else if (strcasecmp($project, 'Expo_2020_Dubai_Opening_Ceremony') == 0){
+            $tagURL = "youtube.com/watch?v=Rb5m8nT7meo";
         } else {
             $tagURL = $default_adkatech_website;
         }
